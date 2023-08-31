51093f63dcb2fbbd624750efb93b96563dcb9dff
khanh1 <hunsolokhanh999@gmail.com>
2023-08-31 20:22:07 +0700

    no message

--- src/screens/welcome/index.tsx
+++ src/screens/welcome/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
-import { AppLogo } from 'assets';
+import { AppLogo } from '../../assets';
 
 const WelcomeScreen = () => {
   return (
