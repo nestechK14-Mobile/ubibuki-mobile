24c626c5954340a6b64ccfdd4214e5cb9e3b98d7
PhongTran <mhphongtran@gmail.com>
2023-08-31 19:27:54 +0700

    2

--- src/screens/welcome/index.tsx
+++ src/screens/welcome/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import styles from './styles';
 import { AppLogo } from 'assets';
+import styles from './../home/styles';
+import styles from './../detail/styles';
 
 const WelcomeScreen = () => {
   return (
     <View style={styles.container}>
       <AppLogo />
+      <Text >Xin chao</Text>
     </View>
   );
 };
