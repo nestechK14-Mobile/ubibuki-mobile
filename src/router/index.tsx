15a191a1ec9b9997934b029fd438394bea6b1239
Kien1509 <66570324+kienwewdwd@users.noreply.github.com>
2023-09-05 22:03:05 +0700

    New_Kien_Updated_9.10.2023

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,21 +1,26 @@
-
+import { View, Text } from 'react-native';
 import React from 'react';
 
-import { HomeScreen, DetailScreen, WelcomeScreen } from 'screens';
-import { SCREEN_NAME } from '../constants';
+import { HomeScreen, DetailScreen, WelcomeScreen, LogInScreen, ShowlistingContentScreen, AboutMoviesScreen } from 'screens';
+import { SCREEN_NAME } from '.././constants';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const MainStack = createNativeStackNavigator();
 
 const MainRouter = () => {
   return (
-    <MainStack.Navigator initialRouteName='false'>
+    <MainStack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}>
+      <MainStack.Screen name={SCREEN_NAME.LOGIN_SCREEN} component={LogInScreen} />
       <MainStack.Screen name={SCREEN_NAME.WELCOME_SCREEN} component={WelcomeScreen} />
       <MainStack.Screen name={SCREEN_NAME.HOME_SCREEN} component={HomeScreen} />
       <MainStack.Screen name={SCREEN_NAME.DETAIL_SCREEN} component={DetailScreen} />
+      <MainStack.Screen name={SCREEN_NAME.SHOWLISTING} component={ShowlistingContentScreen} />
+      <MainStack.Screen name={SCREEN_NAME.ABOUTMOVIES} component={AboutMoviesScreen} />
     </MainStack.Navigator>
   );
 };
 
 export default MainRouter;
-
