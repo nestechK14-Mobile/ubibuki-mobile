dac26ff13ac7b38f22b599d1b7a7f2b79da49a95
Nam Nguyen <namnh98@gmail.com>
2023-09-05 21:18:54 +0700

    revert code

--- src/screens/welcome/index.js
+++ src/screens/welcome/index.js
@@ -1,33 +1,9 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
+import { View } from 'react-native';
 import styles from './styles';
 
 const WelcomeScreen = () => {
-  return (
-    <View style={styles.container}>
-      {/** Header*/}
-      <View style={styles.headerContainer}>
-        <View />
-        <TouchableOpacity>
-          <Text style={styles.titleRightHeader}>SKIP</Text>
-        </TouchableOpacity>
-      </View>
-      {/** Image App Logo*/}
-      <View style={styles.containerAppLogo}>
-        <Image source={require('../../assets/appLogo.png')} />
-      </View>
-      {/** Button Action*/}
-      <View style={styles.containerButtonAction}>
-        <TouchableOpacity style={[styles.buttonDefault]}>
-          <Image source={require('../../assets/google.png')} />
-          <Text style={[styles.titleButtonDefault, { marginLeft: 8 }]}>Log with Google</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.buttonDefault]}>
-          <Text style={styles.titleButtonDefault}>Log with Email</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 };
 
 export default WelcomeScreen;
