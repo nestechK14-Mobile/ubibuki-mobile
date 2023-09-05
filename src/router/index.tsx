18dab149f95b0155b1b0ea427628e4e912101b6b
Kien1509 <66570324+kienwewdwd@users.noreply.github.com>
2023-09-05 22:01:28 +0700

    no message

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,14 +2,14 @@
 import React from 'react';
 
 import { HomeScreen, DetailScreen, WelcomeScreen } from 'screens';
-import { SCREEN_NAME } from 'constants';
+import { SCREEN_NAME } from '../constants';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const MainStack = createNativeStackNavigator();
 
 const MainRouter = () => {
   return (
-    <MainStack.Navigator>
+    <MainStack.Navigator initialRouteName='false'>
       <MainStack.Screen name={SCREEN_NAME.WELCOME_SCREEN} component={WelcomeScreen} />
       <MainStack.Screen name={SCREEN_NAME.HOME_SCREEN} component={HomeScreen} />
       <MainStack.Screen name={SCREEN_NAME.DETAIL_SCREEN} component={DetailScreen} />
