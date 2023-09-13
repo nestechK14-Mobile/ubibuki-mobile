5cbd0197aeacec609ae440f827a0af7181db5de7
Nam Nguyen <namnh98@gmail.com>
2023-09-13 21:02:59 +0700

    NamNH - reinit source base javascript syntax

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 
 const WelcomeScreen = () => {
@@ -8,5 +8,7 @@ const WelcomeScreen = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default WelcomeScreen;
