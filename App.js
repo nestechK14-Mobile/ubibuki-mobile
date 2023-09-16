312a00f16901cc88eb457cce1f7d38926b995c97
Nam Nguyen <namnh98@gmail.com>
2023-09-16 12:18:29 +0700

    NamNH - update to project not config

--- App.js
+++ App.js
@@ -1,47 +1,12 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const MainStack = createNativeStackNavigator();
-
-const DetailScreen = () => {
-  return (
-    <View>
-      <Text>DetailScreen</Text>
-    </View>
-  );
-};
-
-const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  );
-};
-
-const WelcomeScreen = () => {
-  return (
-    <View>
-      <Text>WelcomeScreen</Text>
-    </View>
-
-  );
-};
-
-const styles = StyleSheet.create({});
+import { View, Text } from 'react-native'
+import React from 'react'
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <MainStack.Navigator >
-        <MainStack.Screen name={'WelcomeScreen'} component={WelcomeScreen} />
-        <MainStack.Screen name={'HomeScreen'} component={HomeScreen} />
-        <MainStack.Screen name={'DetailScreen'} component={DetailScreen} />
-      </MainStack.Navigator>
-    </NavigationContainer>
-  );
-};
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
 
-export default App;
+export default App
