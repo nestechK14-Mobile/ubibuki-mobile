4f53c385f5cf78f96effa918194663dac75e80a7
Nam Nguyen <namnh98@gmail.com>
2023-09-16 14:37:01 +0700

    NamNH - config splash screen, scale function, font family custom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainRouter from './router/mainRouter';
+import { Text, TextInput, LogBox } from 'react-native';
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
+
+LogBox.ignoreAllLogs();
+if (!__DEV__) {
+  console.log(() => {});
+}
 
 const App = () => {
   return (
--- src/router/mainRouter.js
+++ src/router/mainRouter.js
@@ -2,10 +2,16 @@ import React from 'react';
 import DummyScreen from '../screens/dummyScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SCREENS_NAME } from '../constants/common';
+import { useLayoutEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 const MainStack = createNativeStackNavigator();
 
 const MainRouter = () => {
+  useLayoutEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <MainStack.Navigator>
       <MainStack.Screen name={SCREENS_NAME.DUMMY_SCREEN} component={DummyScreen} />
