74ae7690de2a415f5079718e2e913af115b1d39c
Nam Nguyen <namnh98@gmail.com>
2023-09-17 19:31:06 +0700

    NamNH - init sample navigation

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Button title="Đến màn Detail" onPress={() => navigation.navigate('Detail')} />
+    </View>
+  );
+};
+
+const DetailScreen = () => {
+  const navigation = useNavigation();
+  return (
+    <View>
+      <Text>DetailScreen</Text>
+      <Button title="Về màn Home" onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
+
+const MainStack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <NavigationContainer>
+      <MainStack.Navigator screenOptions={{ headerShown: false }}>
+        <MainStack.Screen name={'Home'} component={HomeScreen} />
+        <MainStack.Screen name={'Detail'} component={DetailScreen} />
+      </MainStack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default App
+export default App;
