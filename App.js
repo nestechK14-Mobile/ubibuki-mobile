9fc00eb50592addf85d4dcfea248c497c6c26eba
DESKTOP-ACNQCBQ\Admin <tuan.nguyenthanh3796@gmail.com>
2023-09-23 15:07:24 +0700

    no message

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React, { useEffect } from 'react';
-import { BackHandler } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import MainRouter from './src/router/mainRouter';
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
-  }, []);
-
-  useEffect(() => {
-    const backHandler = BackHandler.addEventListener('hardwareBackPress', () => true);
-    return () => backHandler.remove();
   }, []);
 
   return (
