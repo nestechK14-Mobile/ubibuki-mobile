09b1038d8d29942c08ea5c6949ef032e81c4f994
DESKTOP-ACNQCBQ\Admin <tuan.nguyenthanh3796@gmail.com>
2023-09-25 16:18:27 +0700

    no message

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import splash from './src/screens/splash'; // Đảm bảo import màn hình mới của bạn
 
-const App = () => {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="SPLASH" component={splash} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
 export default App;
