f7873423b5d42a04da605149e7b6893604ae6281
DESKTOP-ACNQCBQ\Admin <tuan.nguyenthanh3796@gmail.com>
2023-09-26 18:57:07 +0700

    no message

--- src/screens/01LogoMVT/index.js
+++ src/screens/01LogoMVT/index.js
@@ -1,22 +1,10 @@
-import { View, Image, Text, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, Image, ImageBackground } from 'react-native';
 import React from 'react';
 import styles from '../../screens/01LogoMVT/styles';
-import { useNavigation } from '@react-navigation/core';
-import Login from '../02Login';
 
 const LogoMVT = () => {
-  const navigation = useNavigation();
-  const pressSkip = () => {
-    navigation.navigate(Login);
-  };
   return (
     <ImageBackground style={styles.hinhnen} source={require('../../assets/hinhnen.png')}>
-      <View style={styles.skipbutton}>
-        <TouchableOpacity onPress={pressSkip}>
-          <Text>SKIP</Text>
-        </TouchableOpacity>
-      </View>
-
       <View style={styles.LogoMoVie}>
         <Image source={require('../../assets/LogoTimeMovie.png')} />
       </View>
--- src/screens/01LogoMVT/styles.js
+++ src/screens/01LogoMVT/styles.js
@@ -1,5 +1,12 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  hinhnen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F84464'
+  }
+});
 
 export default styles;
