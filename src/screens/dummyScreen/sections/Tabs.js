cbccb213152d630a34d129d474f9c5d84fbba4ab
Nam Nguyen <namnh98@gmail.com>
2023-09-26 20:41:43 +0700

    NamNH - commit tab in detail screen

--- src/screens/dummyScreen/sections/Tabs.js
+++ src/screens/dummyScreen/sections/Tabs.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 import AboutMovie from '../components/aboutMovie';
 import ShowListing from '../components/showListing';
 
@@ -14,33 +14,58 @@ const Tabs = () => {
     setTabCurrent(item);
   };
 
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity
+        key={item.id}
+        style={{
+          paddingHorizontal: 16,
+          borderBottomWidth: 1,
+          borderBottomColor: tabCurrent.id === item.id ? 'red' : 'transparent',
+          paddingBottom: 8,
+          marginHorizontal: 8
+        }}
+        onPress={() => changeTab(item)}>
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <>
-      <View style={{ flexDirection: 'row', paddingTop: 16 }}>
-        {tab.map(item => {
-          return (
-            <TouchableOpacity
-              key={item.id}
-              style={{
-                paddingHorizontal: 16,
-                borderBottomWidth: 1,
-                borderBottomColor: tabCurrent.id === item.id ? 'red' : 'white',
-                paddingBottom: 8,
-                marginHorizontal: 8
-              }}
-              onPress={() => changeTab(item)}>
-              <Text>{item.name}</Text>
-            </TouchableOpacity>
-          );
-        })}
-      </View>
-      <View style={{ flex: 1 }}>
-        {tabCurrent.name === 'Showlisting' ? <ShowListing /> : <AboutMovie />}
-      </View>
-    </>
+    // <>
+    //   <View style={{ flexDirection: 'row', paddingTop: 16 }}>
+    //     {tab.map(item => {
+    //       return (
+    //         <TouchableOpacity
+    //           key={item.id}
+    //           style={{
+    //             paddingHorizontal: 16,
+    //             borderBottomWidth: 1,
+    //             borderBottomColor: tabCurrent.id === item.id ? 'red' : 'transparent',
+    //             paddingBottom: 8,
+    //             marginHorizontal: 8
+    //           }}
+    //           onPress={() => changeTab(item)}>
+    //           <Text>{item.name}</Text>
+    //         </TouchableOpacity>
+    //       );
+    //     })}
+    //   </View>
+    //   <View style={{ flex: 1 }}>
+    //     {tabCurrent.name === 'Showlisting' ? <ShowListing /> : <AboutMovie />}
+    //   </View>
+    // </>
+    <View>
+      <FlatList
+        data={tab}
+        renderItem={renderItem}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item, index) => item.id}
+      />
+      {tabCurrent.name === 'Showlisting' ? <ShowListing /> : <AboutMovie />}
+    </View>
   );
 };
 
 export default Tabs;
-
-const styles = StyleSheet.create({});
