81621c90351ebd64ebf62c81640b4225c747332b
DESKTOP-ACNQCBQ\Admin <tuan.nguyenthanh3796@gmail.com>
2023-09-27 16:14:37 +0700

    add splash creen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
 import LogoMVT from './src/screens/01LogoMVT';
 import Login from './src/screens/02Login';
 import AfterLogin from './src/screens/03AfterLogin';
@@ -13,6 +14,9 @@ import ChooseOption from './src/screens/08ChooseOption';
 const MainStack = createNativeStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <NavigationContainer>
       <MainStack.Navigator screenOptions={{ headerShown: false }}>
