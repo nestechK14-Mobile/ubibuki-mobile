8b30db1dec11ef69565de6d6c45dea3a5d29654f
MinhTuan89 <tuanbk89@gmail.com>
2023-09-28 18:34:44 +0700

    Test bottomTAB

--- src/screens/screen6.js
+++ src/screens/screen6.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 const Screen6 = () => {
   return (
     <View>
-      <Text>screen6</Text>
+      <Text>screen6 -- Test BOTTOM TAB</Text>
     </View>
   );
 };
