0641ebd7bdbb91da7a1ae5be5b1817a1676c4703
DESKTOP-ACNQCBQ\Admin <tuan.nguyenthanh3796@gmail.com>
2023-10-18 12:12:52 +0700

    Flatlist

--- src/screens/04InputNumber/index.js
+++ src/screens/04InputNumber/index.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image, FlatList } from 'react-native';
 import styles from '../04InputNumber/styles';
 
-const data = [
-  { id: '1', source: require('../../assets/HinhPhimScroll.png') },
-  { id: '2', source: require('../../assets/HinhPhimScroll.png') },
-  { id: '3', source: require('../../assets/HinhPhimScroll.png') }
-];
+const ImageList = () => {
+  const [images, setImages] = useState([
+    { id: '1', src: require('../../assets/HinhPhimScroll.png') },
+    { id: '2', src: require('../../assets/HinhPhimScroll.png') },
+    { id: '3', src: require('../../assets/HinhPhimScroll.png') }
+  ]);
+  const renderItem = ({ item }) => {
+    return (
+      <View style={{ margin: 10 }}>
+        <Image source={item.src} style={{ width: 380, height: 200 }} />
+      </View>
+    );
+  };
 
-const InputNumber = () => {
   return (
     <View>
       <View style={styles.header}>
-        <Image source={require('../../assets/MoviesTimes.png')} />
+        <Image source={require('../../assets/LogoTimeMovie.png')} />
         <Image source={require('../../assets/SearchIcon.png')} />
       </View>
-      <FlatList>
-        data={data}
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => <Image source={item.source} style={styles.image} />}
-      </FlatList>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <FlatList
+          data={images}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          horizontal={true}
+        />
+      </View>
     </View>
   );
 };
-
-export default InputNumber;
+export default ImageList;
